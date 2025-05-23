f157ec76b38971746eacb32e9cdccaf53714628e
MORENA YAEL ZALCMAN <49089410@ort>
2025-05-23 13:40:51 -0300

    view

--- obj/Debug/net8.0/TP04_Ahorcado.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP04_Ahorcado.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP04_Ahorcado")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67ae71ddaf9f992b24733e95e71874897eeebe73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbda5cc6e6941c106c3a3e86fcc2ba2fa0068ad4")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP04_Ahorcado")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP04_Ahorcado")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
